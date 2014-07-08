1a874d6d3f1c71459b98e34017a7b5de89630e86
scarletsky <scarletsky1025@gmail.com>
2014-07-08 22:41:15 +0800

    chore: add with_source flag in getCharactersByKeyword api

--- app/controllers/character.js
+++ app/controllers/character.js
@@ -103,16 +103,57 @@ exports.getCharactersByKeyword = function (req, res) {
   var keyword = req.query.kw;
   var regexpKeyword = new RegExp('.*' + keyword + '.*');
 
-  Character
-    .find({
-      $or: [
-        {name: regexpKeyword},
-        {alias: regexpKeyword}
-      ]
-    })
-    .exec(function (err, characters) {
+  if (req.query.with_source) {
+    async.waterfall([
+      function (callback) {
+        Character
+          .find({
+            $or: [
+              {name: regexpKeyword},
+              {alias: regexpKeyword}
+            ]
+          })
+          .lean()
+          .exec(function (err, characters) {
+            callback(null, characters);
+          });
+      },
+      function (characters, callback) {
+        if (characters.length > 0) {
+          var charactersWithSource = [];
+          async.eachSeries(characters, function (character, callback) {
+            Source
+              .findById(character.sourceId)
+              .lean()
+              .exec(function (err, source) {
+                delete character.sourceId;
+                character.source = source;
+                charactersWithSource.push(character);
+                callback();
+              });
+          }, function (err) {
+            callback(null, charactersWithSource);
+          });
+        } else {
+          callback(null, []);
+        }
+      }
+    ], function (err, characters) {
       return res.send(characters);
     });
+
+  } else {
+    Character
+      .find({
+        $or: [
+          {name: regexpKeyword},
+          {alias: regexpKeyword}
+        ]
+      })
+      .exec(function (err, characters) {
+        return res.send(characters);
+      });
+  }
 };
 
 exports.getCharactersBySourceId = function (req, res) {
