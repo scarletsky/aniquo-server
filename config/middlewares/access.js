6d568af32da1583e723923dc0b5f34d9714a4ebf
scarletsky <scarletsky1025@gmail.com>
2014-08-24 23:01:14 +0800

    chore: add allow ips for POST and PUT method

--- config/middlewares/access.js
+++ config/middlewares/access.js
@@ -1,9 +1,12 @@
+var env = process.env.NODE_ENV || 'development';
+var config = require('../config')[env];
+var allowIps = config.allowIps;
+
 exports.allowCORS = function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-  // intercept OPTIONS method
   if ('OPTIONS' == req.method) {
     res.send(200);
   }
@@ -11,3 +14,11 @@ exports.allowCORS = function (req, res, next) {
     next();
   }
 };
+
+exports.allowAccess = function (req, res, next) {
+  if (allowIps.indexOf(req.ip) === -1) {
+    res.send(403);
+  } else {
+    next();
+  }
+}
