7fdcc7307b93328093df5fdc16bee196d7959730
scarletsky <scarletsky1025@gmail.com>
2014-09-24 16:30:31 +0800

    refactor: sources are sorted by createdAt field in mongo and elasticsearch

--- app/models/source.js
+++ app/models/source.js
@@ -12,7 +12,7 @@ var SourceSchema = new Schema({
   alias: {type: Array, es_indexed: true},
   info: {type: String},
   contributorId: {type: ObjectId, es_indexed: true},
-  createdAt: {type: Date, default: Date.now}
+  createdAt: {type: Date, default: Date.now, es_type: 'date', es_indexed: true}
 });
 
 SourceSchema.index({name: 1, alias: 1});
