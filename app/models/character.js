07d0884d3b835953e40207dbd5333f160a7a4e7b
scarletsky <scarletsky1025@gmail.com>
2014-09-25 12:41:11 +0800

    chore: use elasticsearch in character.js

--- app/models/character.js
+++ app/models/character.js
@@ -5,17 +5,19 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
+var mongoosastic = require('mongoosastic');
 
 var CharacterSchema = new Schema({
-  name: {type: String},
-  alias: {type: Array},
+  name: {type: String, es_indexed: true},
+  alias: {type: Array, es_indexed: true},
   info: {type: String, default: ''},
-  sourceId: {type: ObjectId},
-  quotesCount: {type: Number, default: 0},
-  contributorId: {type: ObjectId},
-  createdAt: {type: Date, default: Date.now}
+  sourceId: {type: ObjectId, es_indexed: true},
+  quotesCount: {type: Number, default: 0, es_indexed: true},
+  contributorId: {type: ObjectId, es_indexed: true},
+  createdAt: {type: Date, default: Date.now, es_indexed: true}
 });
 
 CharacterSchema.index({name: 1, alias: 1});
+CharacterSchema.plugin(mongoosastic)
 
 mongoose.model('Character', CharacterSchema);
